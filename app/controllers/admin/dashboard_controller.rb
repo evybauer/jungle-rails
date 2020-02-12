55bca93f52643012529dcebef7e8f66fa9c1697e
Evelyn Bauer <evybauer@gmail.com>
2020-02-12 03:10:18 +0000

    Fix authentication bug, only logged users can access dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,12 @@
 class Admin::DashboardController < ApplicationController
+
+  http_basic_authenticate_with name: ENV["USERNAME"].to_s, password: ENV["PASSWORD"].to_s
+
+  before_filter :authorize
+
   def show
     @product_count = Product.count
     @category_count = Product.group(:category_id).count
   end
+
 end
